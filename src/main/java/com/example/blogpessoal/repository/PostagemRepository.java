733a2d10f57a814947ceed15b27807e73e7a601a
kel2203 <raquelsb2001@gmail.com>
2023-10-02 11:34:10 -0300

    Post, Put and Delete

--- src/main/java/com/example/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/example/blogpessoal/repository/PostagemRepository.java
@@ -2,6 +2,10 @@ package com.example.blogpessoal.repository;
 
 import com.example.blogpessoal.model.Postagem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+    List<Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
 }
